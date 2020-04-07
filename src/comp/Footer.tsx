3d517e55d7c161895fd3fe71ef812d1a7aae2e7c
unios103 <unios103@gmail.com>
2020-04-07 18:01:43 +0900

    フッター部分のcssを変更

--- src/comp/Footer.tsx
+++ src/comp/Footer.tsx
@@ -5,7 +5,6 @@ import { Divider } from "@material-ui/core";
 interface footerListBase {
   title: string;
   contents: string[];
-  inList?: footerList[];
 }
 interface footerList {
   list: footerListBase[];
@@ -24,17 +23,6 @@ const data: footerList = {
     {
       title: "ユーザーを探す",
       contents: ["名前から探す"],
-      // 例↓
-      inList: [
-        {
-          list: [
-            {
-              title: "いいねした人",
-              contents: ["一覧"],
-            },
-          ],
-        },
-      ],
     },
   ],
 };
@@ -45,14 +33,7 @@ const unfoldingFooterList = (dataList: footerList) => {
       {dataList.list.map((value: footerListBase, i: number) => {
         return (
           <div className={styles.footer_row} key={i}>
-            <div className={styles.footer_row_title}>{value.title}</div>
-            <Divider light />
-            <ul className={styles.footer_row_content}>
-              {value.contents?.map((contents: string, index: number) => {
-                return <li key={index}>・{contents}</li>;
-              })}
-            </ul>
-            {value.inList !== null ? unfoldingInList(value) : ""}
+            {unfoldingContents(value)}
           </div>
         );
       })}
@@ -60,14 +41,18 @@ const unfoldingFooterList = (dataList: footerList) => {
   );
 };
 
-const unfoldingInList = (value: footerListBase) => {
-  return value.inList?.map((list: footerList, inListIndex: number) => {
-    return (
-      <div className={styles.inList_contents_wrapper} key={inListIndex}>
-        {unfoldingFooterList(list)}
-      </div>
-    );
-  });
+const unfoldingContents = (value: footerListBase) => {
+  return (
+    <React.Fragment>
+      <div className={styles.footer_row_title}>{value.title}</div>
+      <Divider light />
+      <ul className={styles.footer_row_content}>
+        {value.contents?.map((contents: string, index: number) => {
+          return <li key={index}>・{contents}</li>;
+        })}
+      </ul>
+    </React.Fragment>
+  );
 };
 
 const Footer = () => {
