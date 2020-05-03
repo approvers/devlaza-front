e1ea741942873b97e5795d0d5edc7bd045fcff13
unios103 <unios103@gmail.com>
2020-05-03 15:24:11 +0900

    引数の受け取り方を変更した

--- src/comp/shareButtonData/data.tsx
+++ src/comp/shareButtonData/data.tsx
@@ -34,33 +34,33 @@ type UrlProps = {
   introduction: string;
   title: string;
 };
-export const Url = (props: UrlProps) => {
+export const Url = ({url,introduction,title}: UrlProps) => {
   const sentenceLengthLimits = 40;
-  const intro = props.introduction.substr(0, sentenceLengthLimits);
+  const intro = introduction.substr(0, sentenceLengthLimits);
   const iconsUrl: Icon = {
     Twitter: {
       definition: faTwitter,
-      url: `https://twitter.com/share?url=${props.url}&text=${intro}`,
+      url: `https://twitter.com/share?url=${url}&text=${intro}`,
     },
     Facebook: {
       definition: faFacebookF,
-      url: `http://www.facebook.com/share.php?u=${props.url}`,
+      url: `http://www.facebook.com/share.php?u=${url}`,
     },
     LINE: {
       definition: faLine,
-      url: `https://social-plugins.line.me/lineit/share?url=${props.url}`,
+      url: `https://social-plugins.line.me/lineit/share?url=${url}`,
     },
     Mail: {
       definition: faEnvelope,
-      url: `mailto:?body=${props.url}`,
+      url: `mailto:?body=${url}`,
     },
     Pocket: {
       definition: faGetPocket,
-      url: `http://getpocket.com/edit?url=${props.url}&title=${props.title}`,
+      url: `http://getpocket.com/edit?url=${url}&title=${title}`,
     },
     Copy: {
       definition: faLink,
-      url: props.url,
+      url: url,
     },
   };
   return iconsUrl;
