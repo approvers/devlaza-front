a76abbc6024ce217e5e8bcef63f044ba5b955e0e
loxygenk <loxygen.k@gmail.com>
2020-05-05 00:44:53 +0900

    †追加† APIを叩いたときの挙動に対応した

--- src/comp/UserInputField.tsx
+++ src/comp/UserInputField.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Chip, TextField } from "@material-ui/core";
-import { Autocomplete } from "@material-ui/lab";
+import { Autocomplete, createFilterOptions } from "@material-ui/lab";
 import Avatar from "@material-ui/core/Avatar";
 
 import * as styles from "css/comp/UserInputField.module.css";
@@ -16,51 +16,114 @@ type UserInputFieldProps = {
 };
 
 type UserInputFieldState = {
-  textFieldValue: string;
-  selectedUser: User[];
+  pendingUsername: string;
   suggestedUser: User[];
+  loading: boolean;
 };
 
 class UserInputField extends React.Component<
   UserInputFieldProps,
   UserInputFieldState
 > {
+  private pendingRequestTimerId: number | null;
+
   constructor(props: UserInputFieldProps) {
     super(props);
 
     this.state = {
-      textFieldValue: "",
-      selectedUser: [],
-      suggestedUser: [
-        {
-          id: "higuchi",
-          name: "H.ichiyo",
-          imageUrl: `${process.env.PUBLIC_URL}/higuchi.png`,
-        },
-        {
-          id: "ichiyo",
-          name: "Ichiyo.h",
-          imageUrl: `${process.env.PUBLIC_URL}/higuchi.png`,
-        },
-      ],
+      pendingUsername: "",
+      suggestedUser: [],
+      loading: false,
     };
+    this.pendingRequestTimerId = null;
   }
 
+  handleUserSelectChange = (value: User[]) => {
+    this.setState({
+      suggestedUser: [],
+    });
+    this.props.onChange(value.map((e) => e.id));
+  };
+
+  handlePendingInputChange = (value: string) => {
+    this.setState({
+      pendingUsername: value,
+    });
+
+    if (this.pendingRequestTimerId != null) {
+      clearTimeout(this.pendingRequestTimerId);
+    }
+
+    if (value.length === 0 || this.state.loading) {
+      this.setState({
+        suggestedUser: [],
+      });
+      return;
+    }
+
+    this.pendingRequestTimerId = setTimeout(
+      this.launchSuggestionRequest,
+      1000,
+      value
+    );
+  };
+
+  launchSuggestionRequest = () => {
+    this.setState({
+      loading: true,
+      suggestedUser: [],
+    });
+    (async () => {
+      // TODO: ここでAPIをぶっ叩いて、候補となるユーザーを提示してもらう
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      this.setState({
+        loading: false,
+        suggestedUser: [
+          {
+            id: "unlimit-highchi",
+            name: "H.ichiyo",
+            imageUrl: `${process.env.PUBLIC_URL}/higuchi.png`,
+          },
+          {
+            id: "dev-ichiyo",
+            name: "Ichiyo.h",
+            imageUrl: `${process.env.PUBLIC_URL}/higuchi.png`,
+          },
+        ],
+      });
+    })();
+  };
+
   render() {
+    const isSearchingById = this.state.pendingUsername.startsWith("@");
+
+    const filterOptions = createFilterOptions({
+      ignoreCase: true,
+      matchFrom: "start",
+      stringify: (option: User) =>
+        isSearchingById ? "@" + option.id : option.name,
+    });
+
     return (
       <Autocomplete
         id="user-name"
-        debug
         filterSelectedOptions
         fullWidth
         multiple
-        clearOnEscape
+        forcePopupIcon={false}
+        loading={this.state.loading}
+        loadingText="読み込み中です…"
+        noOptionsText="見つかりませんでした"
+        filterOptions={filterOptions}
+        getOptionLabel={(option) => option.name}
+        getOptionSelected={(option, value) => option.id === value.id}
+        onInputChange={(_, value) => {
+          this.handlePendingInputChange(value);
+        }}
         onChange={(_, value) => {
-          this.props.onChange(value.map((e) => e.id));
+          this.handleUserSelectChange(value);
         }}
         options={this.state.suggestedUser}
-        getOptionLabel={(option) => option.name}
-        noOptionsText="該当するユーザーは存在しません"
         renderTags={(value: User[], getTagProps) => (
           <>
             {value.map((option: User, index: number) => (
